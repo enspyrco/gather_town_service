05636d1c03812310e8ddddcca249541a7d03825a
Nick Meinhold <nick.meinhold@gmail.com>
2021-05-30 20:28:10 +1000

    Update function to use a global to store the map

--- lib/functions.dart
+++ lib/functions.dart
@@ -5,56 +5,82 @@ import 'package:functions_framework/functions_framework.dart';
 import 'package:http/http.dart' as http;
 import 'package:shelf/shelf.dart' as shelf;
 
-import 'originalJson.dart';
-import 'updatedJson.dart';
+const deltaFunctions = {'showGame': _showGame, 'hideGame': _hideGame};
 
-final client = http.Client();
+final globalClient = http.Client();
+late final getUri;
+final setUri = Uri.https('gather.town', '/api/setMap');
+late final globalQueryParameters = <String, Object?>{};
+var globalMap = <String, Object?>{};
+final List<Map<String, Object?>> removedTrees = [];
 
 @CloudFunction()
 Future<shelf.Response> function(shelf.Request request) async {
-  // return _getMap('saved.json', queryParameters);
-
   try {
-    // Get the gather.town credentials - from local file if running locally
-    // or from environment variable if in Cloud Run.
-    final envVars = Platform.environment;
-    final credentials = envVars['CREDENTIALS'];
-    final queryParameters = (credentials != null)
-        ? json.decode(credentials)
-        : json.decode(File('credentials.json').readAsStringSync());
+    await _setGlobalsIfEmpty();
 
-    // Retrieve the map name from the query parameters.
-    final mapName =
-        '${request.requestedUri.queryParameters['map'] ?? 'original'}';
-
-    return _setMap(mapName, queryParameters);
+    // Retrieve the delta from the query and call the corresponding function
+    final delta = request.requestedUri.queryParameters['delta'];
+    final applyDelta = deltaFunctions[delta];
+    if (applyDelta != null) {
+      applyDelta();
+      return _setCurrentMap();
+    } else {
+      return shelf.Response.ok('No delta found.');
+    }
   } catch (error, trace) {
     return shelf.Response.ok('Error: $error\n\n$trace');
   }
 }
 
-Future<shelf.Response> _getMap(
-    String fileName, Map<String, dynamic> queryParameters) async {
-  final getUri = Uri.https('gather.town', '/api/getMap', queryParameters);
+// Removes potted plants from the globalMap and saves them to a separate list
+// for later re-insertion.
+void _showGame() {
+  final objects = globalMap['objects'] as List<Map<String, Object?>>;
 
-  final response = await client.get(getUri, headers: {
-    HttpHeaders.contentTypeHeader: 'application/json',
-  });
-  File(fileName).writeAsStringSync(response.body);
-
-  return shelf.Response.ok('Response body saved.');
+  for (final object in objects) {
+    final id = object['id'] as String?;
+    if (id != null && id.contains('Potted Plant')) {
+      removedTrees.add(object);
+    }
+  }
+  for (final removedTree in removedTrees) {
+    objects.remove(removedTree);
+  }
 }
 
-Future<shelf.Response> _setMap(
-    String mapName, Map<String, dynamic> queryParameters) async {
-  final setUri = Uri.https('gather.town', '/api/setMap');
+// Put the potted plants back in the global map
+void _hideGame() {
+  final objects = globalMap['objects'] as List<Map<String, Object?>>;
+  objects.addAll(removedTrees);
+  removedTrees.clear();
+}
 
-  print('Setting to: $mapName map');
-  final mapJson = (mapName == 'original') ? originalJson : updatedJson;
-  queryParameters['mapContent'] = json.decode(mapJson);
+Future<void> _setGlobalsIfEmpty() async {
+  if (globalMap.isEmpty) {
+    // Get the gather.town credentials - from local file if running locally
+    // or from environment variable if in Cloud Run.
+    final credentialsString = Platform.environment['CREDENTIALS'] ??
+        File('credentials.json').readAsStringSync();
+    final credentialsMap = json.decode(credentialsString);
+    getUri = Uri.https('gather.town', '/api/getMap', credentialsMap);
 
-  final response = await client.post(setUri,
-      body: json.encode(queryParameters),
+    // We also keep a global map of query parameters, to be combined with the
+    // current gather.town map data in setMap. Here we add set the credentials.
+    globalQueryParameters.addAll(credentialsMap);
+
+    // Retrieve the current gather.town map & store in a global
+    final response = await globalClient.get(getUri,
+        headers: {HttpHeaders.contentTypeHeader: 'application/json'});
+    globalMap = json.decode(response.body);
+  }
+}
+
+Future<shelf.Response> _setCurrentMap() async {
+  globalQueryParameters['mapContent'] = globalMap;
+
+  final response = await globalClient.post(setUri,
+      body: json.encode(globalQueryParameters),
       headers: {HttpHeaders.contentTypeHeader: 'application/json'});
 
   return shelf.Response.ok(response.body);
