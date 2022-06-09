af72fb2d21c3562cb95ba74b058e0f8d4e558ac8
Qays Al-Keero <55286922+QaysAyad@users.noreply.github.com>
2022-06-09 06:18:22 +0300

    Fixed port type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from './app.js';
-const PORT = parseInt(parseInt(process.env.PORT)) || 8080;
+const PORT = (process.env.PORT && parseInt(process.env.PORT)) || 8080;
 
 app.listen(PORT, () =>
   console.log(`gather-town-service listening on port ${PORT}`)
